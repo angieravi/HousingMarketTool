baaebfeb5968f15be677a2c47365fb95ba206485
Alexis Giobbi <71287088+alexisgiobbi@users.noreply.github.com>
2024-12-08 23:47:23 -0500

    deleted unneccessary files

--- backend/models/Review.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const ReviewSchema = new mongoose.Schema({
-  renterName: { type: String, required: true },
-  rating: { type: Number, required: true },
-  comment: { type: String, required: true }
-});
-
-module.exports = mongoose.model('Review', ReviewSchema);
